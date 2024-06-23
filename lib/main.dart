097677bec838e6e92fdb63ece491c9f8bf9aab8c
iMega34 <mega2207@outlook.com>
2024-06-22 20:29:56 -0600

    Added themes for the app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:sync_lyrics/themes.dart';
 import 'package:sync_lyrics/routes/router.dart';
 
 void main() => runApp(const ProviderScope(child: MainApp()));
@@ -12,6 +13,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      theme: AppTheme.lightTheme(context),
+      darkTheme: AppTheme.darkTheme(context),
+      themeMode: /* isDarkMode ? ThemeMode.dark : ThemeMode.light */ ThemeMode.light,
       routerConfig: AppRouter.appRouter,
     );
   }
---
+++ lib/themes.dart
@@ -0,0 +1,53 @@
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:sync_lyrics/utils/custom_themes.dart';
+
+class AppTheme {
+  /// Tema claro de la app
+  static ThemeData lightTheme(BuildContext context) {
+    return ThemeData.light().copyWith(
+      scaffoldBackgroundColor: const Color.fromARGB(255, 234, 234, 234),
+      scrollbarTheme: const ScrollbarThemeData(thumbVisibility: WidgetStatePropertyAll(true)),
+      colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 82, 176, 226)),
+      // snackBarTheme: SnackBarThemeData(
+      //   contentTextStyle: GoogleFonts.tenorSans().copyWith(
+      //     fontSize: Theme.of(context).textTheme.titleMedium!.fontSize,
+      //     color: Colors.white
+      //   )
+      // ),
+      textTheme: GoogleFonts.tenorSansTextTheme(),
+      extensions: const [
+        CustomAppTheme(
+          element: Color.fromARGB(255, 234, 234, 234),
+          internalShadow: Color.fromARGB(255, 255, 255, 255),
+          externalShadow: Color.fromARGB(255, 176, 180, 192),
+          accent: Color.fromARGB(255, 255, 64, 129),
+          error: Color.fromARGB(255, 211, 47, 47)
+        )
+      ]
+    );
+  }
+
+  static ThemeData darkTheme(BuildContext context) {
+    return ThemeData.dark().copyWith(
+      scaffoldBackgroundColor: const Color.fromARGB(255, 31, 33, 41),
+      scrollbarTheme: const ScrollbarThemeData(thumbVisibility: WidgetStatePropertyAll(true)),
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color.fromARGB(255, 82, 176, 226),
+        brightness: Brightness.dark
+      ),
+      snackBarTheme: SnackBarThemeData(
+        contentTextStyle: GoogleFonts.tenorSans().copyWith(
+          fontSize: Theme.of(context).textTheme.titleMedium!.fontSize,
+          color: Colors.white
+        )
+      ),
+      textTheme: GoogleFonts.tenorSansTextTheme().apply(
+        bodyColor: Colors.white,
+        displayColor: Colors.white
+      ),
+    );
+  }
+}
