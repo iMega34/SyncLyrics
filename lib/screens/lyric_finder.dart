1778263f483688240764a90da59395de30d769c0
iMega34 <mega2207@outlook.com>
2024-06-29 19:09:21 -0600

    Renamed `LyricsGeneratorScreen` to `LyricsFinderScreen`

--- lib/screens/lyric_finder.dart
+++ lib/screens/lyric_finder.dart
@@ -4,8 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:sync_lyrics/widgets/lyrics_finder/results_list.dart';
 import 'package:sync_lyrics/widgets/lyrics_finder/search_fields.dart';
 
-class LyricsGeneratorScreen extends StatelessWidget {
-  const LyricsGeneratorScreen({super.key});
+class LyricsFinderScreen extends StatelessWidget {
+  /// Display the lyrics finder screen
+  /// 
+  /// The screen is composed of a search field and a list of results
+  const LyricsFinderScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
