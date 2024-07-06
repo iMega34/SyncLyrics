197d47280c756dcf0a676aa8543d947413c123d0
iMega34 <mega2207@outlook.com>
2024-07-05 23:52:45 -0600

    Update `selected` color in `CustomAppTheme`

--- lib/themes.dart
+++ lib/themes.dart
@@ -28,7 +28,7 @@ class AppTheme {
           element: Color.fromARGB(255, 234, 234, 234),
           internalShadow: Color.fromARGB(255, 255, 255, 255),
           externalShadow: Color.fromARGB(255, 176, 180, 192),
-          accent: Color.fromARGB(255, 255, 64, 129),
+          selected: Color.fromARGB(255, 221, 221, 221),
           error: Color.fromARGB(255, 211, 47, 47)
         )
       ]
