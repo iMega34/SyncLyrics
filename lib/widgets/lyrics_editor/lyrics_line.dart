ec3fd95af0783d32e74c590687cfe4fea22bbfd2
iMega34 <mega2207@outlook.com>
2024-07-14 15:12:53 -0600

    Added `Focus` widget to handle line updates when line is selected or deselected

--- lib/widgets/lyrics_editor/lyrics_line.dart
+++ lib/widgets/lyrics_editor/lyrics_line.dart
@@ -13,25 +13,45 @@ class LyricsLine extends ConsumerStatefulWidget {
   /// 
   /// Parameters:
   /// - [index] is whether the line is selected or not
-  /// - [timestampController] is the controller for the timestamp text field
-  /// - [contentController] is the controller for the content text field
   const LyricsLine({
     super.key,
     required this.index,
-    required this.timestampController,
-    required this.contentController
+    required this.timestamp,
+    required this.content
   });
 
   // Class attributes
   final int index;
-  final TextEditingController timestampController;
-  final TextEditingController contentController;
+  final String timestamp;
+  final String content;
 
   @override
   ConsumerState<LyricsLine> createState() => _LyricsLineState();
 }
 
 class _LyricsLineState extends ConsumerState<LyricsLine> {
+  late TextEditingController _timestampController;
+  late TextEditingController _contentController;
+  late FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+    _focusNode.addListener(_handleFocusChange);
+    _timestampController = TextEditingController(text: widget.timestamp);
+    _contentController = TextEditingController(text: widget.content);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_handleFocusChange);
+    _timestampController.dispose();
+    _contentController.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
   /// Get the width of the timestamp text
   /// 
   /// Parameters:
@@ -41,16 +61,29 @@ class _LyricsLineState extends ConsumerState<LyricsLine> {
   /// - The width of the text as a [double]
   double _getTimestampTextWidth(BuildContext context) {
     // Return 0 if the text is empty
-    if (widget.timestampController.text.isEmpty) return 0;
+    if (_timestampController.text.isEmpty) return 0;
 
     // Create a text painter to measure the width of the text
     final textPainter = TextPainter(
-      text: TextSpan(text: widget.timestampController.text),
+      text: TextSpan(text: _timestampController.text),
       textDirection: TextDirection.ltr,
       maxLines: 1
     )..layout(maxWidth: double.infinity);
 
     return textPainter.width * 1.4;
+  }
+
+  /// Handle the focus change event
+  /// 
+  /// Selects the line when the text field is focused and saves the line when
+  /// the text field is unfocused.
+  void _handleFocusChange() {
+    _focusNode.hasFocus
+      ? ref.read(workspaceProvider.notifier).selectLine(widget.index)
+      : ref.read(workspaceProvider.notifier).saveLine(
+          widget.index,
+          {_timestampController.text: _contentController.text}
+        );
   }
 
   @override
@@ -61,59 +94,58 @@ class _LyricsLineState extends ConsumerState<LyricsLine> {
     final selectedLine = ref.watch(workspaceProvider).selectedLine;
     final isSelected = selectedLine == widget.index;
     final timestampTextWidth = _getTimestampTextWidth(context);
-    return LayoutBuilder(
-      builder: (context, constraints) => TapRegion(
-        onTapInside: (_) => ref.read(workspaceProvider.notifier).selectLine(widget.index),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            AnimatedContainer(
-              curve: Curves.linearToEaseOut,
-              duration: const Duration(milliseconds: 500),
-              margin: isSelected ? const EdgeInsets.symmetric(vertical: 20) : const EdgeInsets.symmetric(),
-              padding: isSelected ? const EdgeInsets.symmetric(horizontal: 10, vertical: 20) : const EdgeInsets.symmetric(horizontal: 10),
-              decoration: BoxDecoration(
-                borderRadius: isSelected ? BorderRadius.circular(10) : BorderRadius.circular(0),
-                color: isSelected ? selectedColor : backgroundColor
-              ),
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: timestampTextWidth,
-                    child: TextField(
-                      controller: widget.timestampController,
-                      decoration: const InputDecoration(border: InputBorder.none, isDense: true, counterText: ""),
-                      maxLength: 10,
-                    )
-                  ),
-                  const SizedBox(width: 5),
-                  Expanded(
-                    child: TextField(
-                      controller: widget.contentController,
-                      decoration: const InputDecoration(border: InputBorder.none, isDense: true)
-                    )
+
+    return Focus(
+      focusNode: _focusNode,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          AnimatedContainer(
+            curve: Curves.linearToEaseOut,
+            duration: const Duration(milliseconds: 500),
+            margin: isSelected ? const EdgeInsets.symmetric(vertical: 20) : const EdgeInsets.symmetric(),
+            padding: isSelected ? const EdgeInsets.symmetric(horizontal: 10, vertical: 20) : const EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+              borderRadius: isSelected ? BorderRadius.circular(10) : BorderRadius.circular(0),
+              color: isSelected ? selectedColor : backgroundColor
+            ),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: timestampTextWidth,
+                  child: TextField(
+                    controller: _timestampController,
+                    decoration: const InputDecoration(border: InputBorder.none, isDense: true, counterText: ""),
+                    maxLength: 10,
                   )
-                ],
-              ),
+                ),
+                const SizedBox(width: 5),
+                Expanded(
+                  child: TextField(
+                    controller: _contentController,
+                    decoration: const InputDecoration(border: InputBorder.none, isDense: true)
+                  )
+                )
+              ],
             ),
-            // Display the button row when the line is selected
-            Positioned(
-              top: 10,
-              child: AnimatedSwitcher(
-                duration: const Duration(milliseconds: 350),
-                child: isSelected ? const ButtonRow() : const SizedBox.shrink(),
-              ),
+          ),
+          // Display the button row when the line is selected
+          Positioned(
+            top: 10,
+            child: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 350),
+              child: isSelected ? const ButtonRow() : const SizedBox.shrink(),
             ),
-            Positioned(
-              bottom: 10,
-              child: AnimatedSwitcher(
-                duration: const Duration(milliseconds: 350),
-                child: isSelected ? const ButtonRow(lowerRow: true) : const SizedBox.shrink(),
-              ),
-            )
-          ],
-        ),
-      )
+          ),
+          Positioned(
+            bottom: 10,
+            child: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 350),
+              child: isSelected ? const ButtonRow(lowerRow: true) : const SizedBox.shrink(),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
