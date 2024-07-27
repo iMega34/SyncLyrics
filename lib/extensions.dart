5aff313c2c2090e027d5daa986afbb55835b65d0
iMega34 <mega2207@outlook.com>
2024-07-26 21:43:48 -0600

    Implemented `textWidth` getter as an extension of the `String` class

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,4 +1,6 @@
 
+import 'package:flutter/material.dart';
+
 /// Extensions for Dart's built-in [bool] class.
 extension BoolToInt on bool {
   /// Converts a boolean value into its integer representation.
@@ -9,4 +11,22 @@ extension BoolToInt on bool {
 extension MapEntryToRecord<K, V> on MapEntry<K, V> {
   /// Converts a [MapEntry] into a record for easy value retrieval.
   (K key, V value) get record => (key, value);
+}
+
+/// Extensions for Dart's built-in [String] class.
+extension TextWidth on String {
+  /// Get the width of a text when rendered on the screen as a [double].
+  double get textWidth {
+    // Return 0 if the text is empty
+    if (isEmpty) return 0;
+
+    // Create a text painter to measure the text width
+    final textPainter = TextPainter(
+      text: TextSpan(text: this),
+      textDirection: TextDirection.ltr,
+      maxLines: 1
+    )..layout(maxWidth: double.infinity);
+
+    return textPainter.width;
+  }
 }
