ba5b876db6ab8530d5222d4865cd735df809bf6c
iMega34 <mega2207@outlook.com>
2024-07-30 00:55:12 -0600

    Refactor `SearchFields` to include error handling and display `CustomSnackBar`. Fix data persistency issue in `NeumorphicTextField`

--- lib/widgets/lyrics_finder/search_fields.dart
+++ lib/widgets/lyrics_finder/search_fields.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:sync_lyrics/utils/custom_snack_bar.dart';
 import 'package:sync_lyrics/utils/neumorphic/neumorphic_button.dart';
 import 'package:sync_lyrics/utils/neumorphic/neumorphic_text_field.dart';
 import 'package:sync_lyrics/providers/musixmatch_results_provider.dart';
@@ -15,25 +16,57 @@ class SearchFields extends ConsumerStatefulWidget {
 }
 
 class _SearchFieldsState extends ConsumerState<SearchFields> {
-  late TextEditingController _trackController = TextEditingController();
-  late TextEditingController _artistController = TextEditingController();
+  late TextEditingController _artistController;
+  late TextEditingController _trackController;
 
   @override
   void initState() {
     super.initState();
-    _trackController = TextEditingController();
-    _artistController = TextEditingController();
+    final searchFields = ref.read(musixmatchResultsProvider);
+    _artistController = TextEditingController(text: searchFields.artist);
+    _trackController = TextEditingController(text: searchFields.track);
   }
 
   @override
   void dispose() {
-    _trackController.dispose();
     _artistController.dispose();
+    _trackController.dispose();
     super.dispose();
   }
 
+  /// Handles error status codes
+  /// 
+  /// Displays a [CustomSnackBar] based on the status code to alert the user of an error
+  /// 
+  /// Parameters:
+  /// - [statusCode] is the status code
+  void _handleStatusCode(int statusCode) {
+    switch (statusCode) {
+      case 0:
+        break;
+      case -1:
+        showCustomSnackBar(
+          context,
+          type: SnackBarType.error,
+          title: "Input error",
+          message: "The artist or track name was not provided"
+        );
+        break;
+      case -2:
+        showCustomSnackBar(
+          context,
+          type: SnackBarType.warning,
+          title: "No results",
+          message: "Couldn't find any results for the provided artist and track.\nTry changing the search terms"
+        );
+        break;
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
+    // TODO: Implementar extracción de metadatos como método de búsqueda
+    final notifier = ref.read(musixmatchResultsProvider.notifier);
     return Row(
       children: [
         Expanded(
@@ -41,6 +74,7 @@ class _SearchFieldsState extends ConsumerState<SearchFields> {
             label: "Artist",
             controller: _artistController,
             margin: const EdgeInsets.only(right: 10),
+            onChanged: (String text) => notifier.setArtist(text),
           )
         ),
         Expanded(
@@ -48,6 +82,7 @@ class _SearchFieldsState extends ConsumerState<SearchFields> {
             label: "Track",
             controller: _trackController,
             margin: const EdgeInsets.symmetric(horizontal: 10),
+            onChanged: (String text) => notifier.setTrack(text),
           )
         ),
         NeumorphicButton(
@@ -55,9 +90,8 @@ class _SearchFieldsState extends ConsumerState<SearchFields> {
           margin: const EdgeInsets.only(left: 10),
           padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
           onPressed: () async {
-            final qTrack = _trackController.text;
-            final qArtist = _artistController.text;
-            await ref.read(musixmatchResultsProvider.notifier).searchTracks(qTrack, qArtist);
+            final statusCode = await notifier.searchTracks();
+            _handleStatusCode(statusCode);
           }
         )
       ],
