2ba9a19b489e69a7fbba84059f7198cb52e88876
Erick <mega2207@outlook.com>
2024-08-11 00:00:05 -0600

    Added `Toolbar` widget

--- lib/screens/lyrics_editor.dart
+++ lib/screens/lyrics_editor.dart
@@ -4,32 +4,37 @@ import 'package:flutter/material.dart';
 import 'package:sync_lyrics/utils/neumorphic/neumorphic.dart';
 import 'package:sync_lyrics/widgets/lyrics_editor/workspace.dart';
 import 'package:sync_lyrics/widgets/lyrics_editor/lyrics_info.dart';
+import 'package:sync_lyrics/widgets/lyrics_editor/toolbar.dart';
 
 class LyricsEditorScreen extends StatelessWidget {
   const LyricsEditorScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(15),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Neumorphic(
-              padding: EdgeInsets.all(15),
-              margin: EdgeInsets.only(right: 10),
-              child: Column(
-                children: [
-                  LyricsInfo(),
-                  SizedBox(height: 10),
-                  Workspace()
-                ],
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: FocusScope.of(context).unfocus,
+      child: const Padding(
+        padding: EdgeInsets.all(15),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Neumorphic(
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.only(right: 10),
+                child: Column(
+                  children: [
+                    LyricsInfo(),
+                    SizedBox(height: 10),
+                    Workspace()
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(flex: 1, child: Neumorphic(child: Placeholder()))
-        ],
+            Expanded(flex: 1, child: Toolbar())
+          ],
+        ),
       ),
     );
   }
