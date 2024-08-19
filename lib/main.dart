78877bd89f59a9bbf121405c7351a1ac6c6e1829
Erick <mega2207@outlook.com>
2024-08-19 11:11:48 -0600

    Initialize settings provider in `MainApp`

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'package:sync_lyrics/themes.dart';
 import 'package:sync_lyrics/routes/router.dart';
+import 'package:sync_lyrics/providers/settings_provider.dart';
 
 void main() {
   sqfliteFfiInit();
@@ -12,11 +13,31 @@ void main() {
   runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerStatefulWidget {
   const MainApp({super.key});
 
   @override
+  ConsumerState<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends ConsumerState<MainApp> {
+  bool _isInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeSettings();
+  }
+
+  /// Initialize the settings provider with the local settings database
+  void _initializeSettings() async {
+    await ref.read(settingsProvider.notifier).initializeSettings();
+    setState(() => _isInitialized = true);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (!_isInitialized) return const CircularProgressIndicator();
     return MaterialApp.router(
       theme: AppTheme.lightTheme(context),
       darkTheme: AppTheme.darkTheme(context),
